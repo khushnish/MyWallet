752db358bf96b01f2a5b4ef91442120578f51842
khushnish <khushnishshah@gmail.com>
2013-12-13 16:15:51 +0530

    Update CardModel.java

--- src/com/khushnish/mywallet/model/CardModel.java
+++ src/com/khushnish/mywallet/model/CardModel.java
@@ -7,25 +7,25 @@ public class CardModel implements Parcelable {
 
 	private long id = 0;
 	// Visa, Master, Mastro, Other
-	private int cardType = 0;
+	private String cardType = "";
 	private String otherCardName = "";
 	// Personal, Business
-	private int cardUserType = 0;
+	private String cardUserType = "";
 	private String bankName = "";
 	private String bankAccountNumber = "";
 	private String bankCustomerId = "";
 	private String cardNumber = "";
 	private String cardHolderName = "";
-	private int cardCvvNumber = 0;
-	private int cardAtmPinNumber = 0;
+	private String cardCvvNumber = "";
+	private String cardAtmPinNumber = "";
 	private String cardTransactionPassword = "";
-	private int validFromMonth = 0;
-	private int validFromYear = 0;
-	private int validTillMonth = 0;
-	private int validTillYear = 0;
+	private String validFromMonth = "";
+	private String validFromYear = "";
+	private String validTillMonth = "";
+	private String validTillYear = "";
 	private String imageFront = "";
 	private String imageBack = "";
-	private int bankCardMobilePinNumber = 0;
+	private String bankCardMobilePinNumber = "";
 	
 	public CardModel() {
 	}
@@ -38,11 +38,11 @@ public class CardModel implements Parcelable {
 		this.id = id;
 	}
 
-	public int getCardType() {
+	public String getCardType() {
 		return cardType;
 	}
 
-	public void setCardType(int cardType) {
+	public void setCardType(String cardType) {
 		this.cardType = cardType;
 	}
 
@@ -54,11 +54,11 @@ public class CardModel implements Parcelable {
 		this.otherCardName = otherCardName;
 	}
 
-	public int getCardUserType() {
+	public String getCardUserType() {
 		return cardUserType;
 	}
 
-	public void setCardUserType(int cardUserType) {
+	public void setCardUserType(String cardUserType) {
 		this.cardUserType = cardUserType;
 	}
 
@@ -102,19 +102,19 @@ public class CardModel implements Parcelable {
 		this.cardHolderName = cardHolderName;
 	}
 
-	public int getCardCvvNumber() {
+	public String getCardCvvNumber() {
 		return cardCvvNumber;
 	}
 
-	public void setCardCvvNumber(int cardCvvNumber) {
+	public void setCardCvvNumber(String cardCvvNumber) {
 		this.cardCvvNumber = cardCvvNumber;
 	}
 
-	public int getCardAtmPinNumber() {
+	public String getCardAtmPinNumber() {
 		return cardAtmPinNumber;
 	}
 
-	public void setCardAtmPinNumber(int cardAtmPinNumber) {
+	public void setCardAtmPinNumber(String cardAtmPinNumber) {
 		this.cardAtmPinNumber = cardAtmPinNumber;
 	}
 
@@ -126,35 +126,35 @@ public class CardModel implements Parcelable {
 		this.cardTransactionPassword = cardTransactionPassword;
 	}
 
-	public int getValidFromMonth() {
+	public String getValidFromMonth() {
 		return validFromMonth;
 	}
 
-	public void setValidFromMonth(int validFromMonth) {
+	public void setValidFromMonth(String validFromMonth) {
 		this.validFromMonth = validFromMonth;
 	}
 
-	public int getValidFromYear() {
+	public String getValidFromYear() {
 		return validFromYear;
 	}
 
-	public void setValidFromYear(int validFromYear) {
+	public void setValidFromYear(String validFromYear) {
 		this.validFromYear = validFromYear;
 	}
 
-	public int getValidTillMonth() {
+	public String getValidTillMonth() {
 		return validTillMonth;
 	}
 
-	public void setValidTillMonth(int validTillMonth) {
+	public void setValidTillMonth(String validTillMonth) {
 		this.validTillMonth = validTillMonth;
 	}
 
-	public int getValidTillYear() {
+	public String getValidTillYear() {
 		return validTillYear;
 	}
 
-	public void setValidTillYear(int validTillYear) {
+	public void setValidTillYear(String validTillYear) {
 		this.validTillYear = validTillYear;
 	}
 
@@ -174,34 +174,34 @@ public class CardModel implements Parcelable {
 		this.imageBack = imageBack;
 	}
 
-	public int getBankCardMobilePinNumber() {
+	public String getBankCardMobilePinNumber() {
 		return bankCardMobilePinNumber;
 	}
 
-	public void setBankCardMobilePinNumber(int bankCardMobilePinNumber) {
+	public void setBankCardMobilePinNumber(String bankCardMobilePinNumber) {
 		this.bankCardMobilePinNumber = bankCardMobilePinNumber;
 	}
 
 	protected CardModel(Parcel in) {
 		id = in.readLong();
-		cardType = in.readInt();
+		cardType = in.readString();
 		otherCardName = in.readString();
-		cardUserType = in.readInt();
+		cardUserType = in.readString();
 		bankName = in.readString();
 		bankAccountNumber = in.readString();
 		bankCustomerId = in.readString();
 		cardNumber = in.readString();
 		cardHolderName = in.readString();
-		cardCvvNumber = in.readInt();
-		cardAtmPinNumber = in.readInt();
+		cardCvvNumber = in.readString();
+		cardAtmPinNumber = in.readString();
 		cardTransactionPassword = in.readString();
-		validFromMonth = in.readInt();
-		validFromYear = in.readInt();
-		validTillMonth = in.readInt();
-		validTillYear = in.readInt();
+		validFromMonth = in.readString();
+		validFromYear = in.readString();
+		validTillMonth = in.readString();
+		validTillYear = in.readString();
 		imageFront = in.readString();
 		imageBack = in.readString();
-		bankCardMobilePinNumber = in.readInt();
+		bankCardMobilePinNumber = in.readString();
 	}
 
 	@Override
@@ -212,24 +212,24 @@ public class CardModel implements Parcelable {
 	@Override
 	public void writeToParcel(Parcel dest, int flags) {
 		dest.writeLong(id);
-		dest.writeInt(cardType);
+		dest.writeString(cardType);
 		dest.writeString(otherCardName);
-		dest.writeInt(cardUserType);
+		dest.writeString(cardUserType);
 		dest.writeString(bankName);
 		dest.writeString(bankAccountNumber);
 		dest.writeString(bankCustomerId);
 		dest.writeString(cardNumber);
 		dest.writeString(cardHolderName);
-		dest.writeInt(cardCvvNumber);
-		dest.writeInt(cardAtmPinNumber);
+		dest.writeString(cardCvvNumber);
+		dest.writeString(cardAtmPinNumber);
 		dest.writeString(cardTransactionPassword);
-		dest.writeInt(validFromMonth);
-		dest.writeInt(validFromYear);
-		dest.writeInt(validTillMonth);
-		dest.writeInt(validTillYear);
+		dest.writeString(validFromMonth);
+		dest.writeString(validFromYear);
+		dest.writeString(validTillMonth);
+		dest.writeString(validTillYear);
 		dest.writeString(imageFront);
 		dest.writeString(imageBack);
-		dest.writeInt(bankCardMobilePinNumber);
+		dest.writeString(bankCardMobilePinNumber);
 	}
 
 	public static final Parcelable.Creator<CardModel> CREATOR = new Parcelable.Creator<CardModel>() {

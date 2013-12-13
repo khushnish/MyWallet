c511908ee3d4252dc2f5c0c5db9332c8bcb4bc17
khushnish <khushnishshah@gmail.com>
2013-12-13 16:14:59 +0530

    Update DatabaseHelper.java

--- src/com/khushnish/mywallet/database/DatabaseHelper.java
+++ src/com/khushnish/mywallet/database/DatabaseHelper.java
@@ -2,6 +2,8 @@ package com.khushnish.mywallet.database;
 
 import java.util.ArrayList;
 
+import javax.crypto.SecretKey;
+
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
@@ -11,14 +13,18 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 import com.khushnish.mywallet.R;
 import com.khushnish.mywallet.model.CardModel;
+import com.khushnish.mywallet.utils.Crypto;
+import com.khushnish.mywallet.utils.Utils;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 	
 	private SQLiteDatabase database;
+	private Encryptor encryptor;
 	
 	public DatabaseHelper(Context context, String name, CursorFactory factory,
 			int version) {
 		super(context, context.getString(R.string.db_name), null, version);
+		encryptor = PADDING_ENCRYPTOR;
 	}
 
 	@Override
@@ -29,7 +35,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 			this.database.execSQL("PRAGMA foreign_keys = ON;");
 		}
 		
-		final String cardDetailsTable = "CREATE TABLE IF NOT EXISTS CardDetails (ID INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE , CardType INTEGER, OtherCardName TEXT, CardUserType INTEGER, BankName TEXT, BankAccountNumber TEXT, BankCustomerId TEXT, CardNumber TEXT, CardHolderName TEXT, CardCVVNumber INTEGER, CardATMPinNumber INTEGER, CardTransactionPassword TEXT, BankMobilePinNumber TEXT, ValidFromMonth INTEGER, ValidFromYear INTEGER, ValidTillMonth INTEGER, ValidTillYear INTEGER, ImageFront TEXT, ImageBack TEXT)";
+		final String cardDetailsTable = "CREATE TABLE IF NOT EXISTS CardDetails (ID INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE , CardType TEXT, OtherCardName TEXT, CardUserType TEXT, BankName TEXT, BankAccountNumber TEXT, BankCustomerId TEXT, CardNumber TEXT, CardHolderName TEXT, CardCVVNumber TEXT, CardATMPinNumber TEXT, CardTransactionPassword TEXT, BankMobilePinNumber TEXT, ValidFromMonth TEXT, ValidFromYear TEXT, ValidTillMonth TEXT, ValidTillYear TEXT, ImageFront TEXT, ImageBack TEXT)";
 		
 		this.database.execSQL(cardDetailsTable);
 	}
@@ -73,24 +79,42 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 					cursor.moveToNext();
 					cardModel = new CardModel();
 					cardModel.setId(cursor.getLong(cursor.getColumnIndex(DBConstants.ID)));
-					cardModel.setCardType(cursor.getInt(cursor.getColumnIndex(DBConstants.COL_CARDDETAILS_CARDTYPE)));
-					cardModel.setOtherCardName(cursor.getString(cursor.getColumnIndex(DBConstants.COL_CARDDETAILS_OTHERCARDNAME)));
-					cardModel.setCardUserType(cursor.getInt(cursor.getColumnIndex(DBConstants.COL_CARDDETAILS_CARDUSERTYPE)));
-					cardModel.setBankName(cursor.getString(cursor.getColumnIndex(DBConstants.COL_CARDDETAILS_BANKNAME)));
-					cardModel.setBankAccountNumber(cursor.getString(cursor.getColumnIndex(DBConstants.COL_CARDDETAILS_BANKACCOUNTNUMBER)));
-					cardModel.setBankCustomerId(cursor.getString(cursor.getColumnIndex(DBConstants.COL_CARDDETAILS_BANKCUSTOMERID)));
-					cardModel.setCardNumber(cursor.getString(cursor.getColumnIndex(DBConstants.COL_CARDDETAILS_CARDNUMBER)));
-					cardModel.setCardHolderName(cursor.getString(cursor.getColumnIndex(DBConstants.COL_CARDDETAILS_CARDHOLDERNAME)));
-					cardModel.setCardCvvNumber(cursor.getInt(cursor.getColumnIndex(DBConstants.COL_CARDDETAILS_CARDCVVNUMBER)));
-					cardModel.setCardAtmPinNumber(cursor.getInt(cursor.getColumnIndex(DBConstants.COL_CARDDETAILS_CARDATMPINNUMBER)));
-					cardModel.setBankCardMobilePinNumber(cursor.getInt(cursor.getColumnIndex(DBConstants.COL_CARDDETAILS_BANKMOBILEPINNUMBER)));
-					cardModel.setCardTransactionPassword(cursor.getString(cursor.getColumnIndex(DBConstants.COL_CARDDETAILS_CARDTRANSACTIONPASSWORD)));
-					cardModel.setValidFromMonth(cursor.getInt(cursor.getColumnIndex(DBConstants.COL_CARDDETAILS_VALIDFROMMONTH)));
-					cardModel.setValidFromYear(cursor.getInt(cursor.getColumnIndex(DBConstants.COL_CARDDETAILS_VALIDFROMYEAR)));
-					cardModel.setValidTillMonth(cursor.getInt(cursor.getColumnIndex(DBConstants.COL_CARDDETAILS_VALIDTILLMONTH)));
-					cardModel.setValidTillYear(cursor.getInt(cursor.getColumnIndex(DBConstants.COL_CARDDETAILS_VALIDTILLYEAR)));
-					cardModel.setImageFront(cursor.getString(cursor.getColumnIndex(DBConstants.COL_CARDDETAILS_IMAGEFRONT)));
-					cardModel.setImageBack(cursor.getString(cursor.getColumnIndex(DBConstants.COL_CARDDETAILS_IMAGEBACK)));
+					cardModel.setCardType(encryptor.decrypt(cursor.getString(cursor.getColumnIndex(
+							DBConstants.COL_CARDDETAILS_CARDTYPE)), Utils.key));
+					cardModel.setOtherCardName(encryptor.decrypt(cursor.getString(cursor.getColumnIndex(
+							DBConstants.COL_CARDDETAILS_OTHERCARDNAME)), Utils.key));
+					cardModel.setCardUserType(encryptor.decrypt(cursor.getString(cursor.getColumnIndex(
+							DBConstants.COL_CARDDETAILS_CARDUSERTYPE)), Utils.key));
+					cardModel.setBankName(encryptor.decrypt(cursor.getString(cursor.getColumnIndex(
+							DBConstants.COL_CARDDETAILS_BANKNAME)), Utils.key));
+					cardModel.setBankAccountNumber(encryptor.decrypt(cursor.getString(cursor.getColumnIndex(
+							DBConstants.COL_CARDDETAILS_BANKACCOUNTNUMBER)), Utils.key));
+					cardModel.setBankCustomerId(encryptor.decrypt(cursor.getString(cursor.getColumnIndex(
+							DBConstants.COL_CARDDETAILS_BANKCUSTOMERID)), Utils.key));
+					cardModel.setCardNumber(encryptor.decrypt(cursor.getString(cursor.getColumnIndex(
+							DBConstants.COL_CARDDETAILS_CARDNUMBER)), Utils.key));
+					cardModel.setCardHolderName(encryptor.decrypt(cursor.getString(cursor.getColumnIndex(
+							DBConstants.COL_CARDDETAILS_CARDHOLDERNAME)), Utils.key));
+					cardModel.setCardCvvNumber(encryptor.decrypt(cursor.getString(cursor.getColumnIndex(
+							DBConstants.COL_CARDDETAILS_CARDCVVNUMBER)), Utils.key));
+					cardModel.setCardAtmPinNumber(encryptor.decrypt(cursor.getString(cursor.getColumnIndex(
+							DBConstants.COL_CARDDETAILS_CARDATMPINNUMBER)), Utils.key));
+					cardModel.setBankCardMobilePinNumber(encryptor.decrypt(cursor.getString(cursor.getColumnIndex(
+							DBConstants.COL_CARDDETAILS_BANKMOBILEPINNUMBER)), Utils.key));
+					cardModel.setCardTransactionPassword(encryptor.decrypt(cursor.getString(cursor.getColumnIndex(
+							DBConstants.COL_CARDDETAILS_CARDTRANSACTIONPASSWORD)), Utils.key));
+					cardModel.setValidFromMonth(encryptor.decrypt(cursor.getString(cursor.getColumnIndex(
+							DBConstants.COL_CARDDETAILS_VALIDFROMMONTH)), Utils.key));
+					cardModel.setValidFromYear(encryptor.decrypt(cursor.getString(cursor.getColumnIndex(
+							DBConstants.COL_CARDDETAILS_VALIDFROMYEAR)), Utils.key));
+					cardModel.setValidTillMonth(encryptor.decrypt(cursor.getString(cursor.getColumnIndex(
+							DBConstants.COL_CARDDETAILS_VALIDTILLMONTH)), Utils.key));
+					cardModel.setValidTillYear(encryptor.decrypt(cursor.getString(cursor.getColumnIndex(
+							DBConstants.COL_CARDDETAILS_VALIDTILLYEAR)), Utils.key));
+					cardModel.setImageFront(encryptor.decrypt(cursor.getString(cursor.getColumnIndex(
+							DBConstants.COL_CARDDETAILS_IMAGEFRONT)), Utils.key));
+					cardModel.setImageBack(encryptor.decrypt(cursor.getString(cursor.getColumnIndex(
+							DBConstants.COL_CARDDETAILS_IMAGEBACK)), Utils.key));
 					cardModels.add(cardModel);
 				}
 			}
@@ -112,24 +136,24 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 		
 		try {
 			final ContentValues values = new ContentValues();
-			values.put(DBConstants.COL_CARDDETAILS_CARDTYPE, cardModel.getCardType());
-			values.put(DBConstants.COL_CARDDETAILS_OTHERCARDNAME, cardModel.getOtherCardName());
-			values.put(DBConstants.COL_CARDDETAILS_CARDUSERTYPE, cardModel.getCardUserType());
-			values.put(DBConstants.COL_CARDDETAILS_BANKNAME, cardModel.getBankName());
-			values.put(DBConstants.COL_CARDDETAILS_BANKACCOUNTNUMBER, cardModel.getBankAccountNumber());
-			values.put(DBConstants.COL_CARDDETAILS_BANKCUSTOMERID, cardModel.getBankCustomerId());
-			values.put(DBConstants.COL_CARDDETAILS_CARDNUMBER, cardModel.getCardNumber());
-			values.put(DBConstants.COL_CARDDETAILS_CARDHOLDERNAME, cardModel.getCardHolderName());
-			values.put(DBConstants.COL_CARDDETAILS_CARDCVVNUMBER, cardModel.getCardCvvNumber());
-			values.put(DBConstants.COL_CARDDETAILS_CARDATMPINNUMBER, cardModel.getCardAtmPinNumber());
-			values.put(DBConstants.COL_CARDDETAILS_BANKMOBILEPINNUMBER, cardModel.getBankCardMobilePinNumber());
-			values.put(DBConstants.COL_CARDDETAILS_CARDTRANSACTIONPASSWORD, cardModel.getCardTransactionPassword());
-			values.put(DBConstants.COL_CARDDETAILS_VALIDFROMMONTH, cardModel.getValidFromMonth());
-			values.put(DBConstants.COL_CARDDETAILS_VALIDFROMYEAR, cardModel.getValidFromYear());
-			values.put(DBConstants.COL_CARDDETAILS_VALIDTILLMONTH, cardModel.getValidTillMonth());
-			values.put(DBConstants.COL_CARDDETAILS_VALIDTILLYEAR, cardModel.getValidTillYear());
-			values.put(DBConstants.COL_CARDDETAILS_IMAGEFRONT, cardModel.getImageFront());
-			values.put(DBConstants.COL_CARDDETAILS_IMAGEBACK, cardModel.getImageBack());
+			values.put(DBConstants.COL_CARDDETAILS_CARDTYPE, encryptor.encrypt(cardModel.getCardType(), Utils.key));
+			values.put(DBConstants.COL_CARDDETAILS_OTHERCARDNAME, encryptor.encrypt(cardModel.getOtherCardName(), Utils.key));
+			values.put(DBConstants.COL_CARDDETAILS_CARDUSERTYPE, encryptor.encrypt(cardModel.getCardUserType(), Utils.key));
+			values.put(DBConstants.COL_CARDDETAILS_BANKNAME, encryptor.encrypt(cardModel.getBankName(), Utils.key));
+			values.put(DBConstants.COL_CARDDETAILS_BANKACCOUNTNUMBER, encryptor.encrypt(cardModel.getBankAccountNumber(), Utils.key));
+			values.put(DBConstants.COL_CARDDETAILS_BANKCUSTOMERID, encryptor.encrypt(cardModel.getBankCustomerId(), Utils.key));
+			values.put(DBConstants.COL_CARDDETAILS_CARDNUMBER, encryptor.encrypt(cardModel.getCardNumber(), Utils.key));
+			values.put(DBConstants.COL_CARDDETAILS_CARDHOLDERNAME, encryptor.encrypt(cardModel.getCardHolderName(), Utils.key));
+			values.put(DBConstants.COL_CARDDETAILS_CARDCVVNUMBER, encryptor.encrypt(cardModel.getCardCvvNumber(), Utils.key));
+			values.put(DBConstants.COL_CARDDETAILS_CARDATMPINNUMBER, encryptor.encrypt(cardModel.getCardAtmPinNumber(), Utils.key));
+			values.put(DBConstants.COL_CARDDETAILS_BANKMOBILEPINNUMBER, encryptor.encrypt(cardModel.getBankCardMobilePinNumber(), Utils.key));
+			values.put(DBConstants.COL_CARDDETAILS_CARDTRANSACTIONPASSWORD, encryptor.encrypt(cardModel.getCardTransactionPassword(), Utils.key));
+			values.put(DBConstants.COL_CARDDETAILS_VALIDFROMMONTH, encryptor.encrypt(cardModel.getValidFromMonth(), Utils.key));
+			values.put(DBConstants.COL_CARDDETAILS_VALIDFROMYEAR, encryptor.encrypt(cardModel.getValidFromYear(), Utils.key));
+			values.put(DBConstants.COL_CARDDETAILS_VALIDTILLMONTH, encryptor.encrypt(cardModel.getValidTillMonth(), Utils.key));
+			values.put(DBConstants.COL_CARDDETAILS_VALIDTILLYEAR, encryptor.encrypt(cardModel.getValidTillYear(), Utils.key));
+			values.put(DBConstants.COL_CARDDETAILS_IMAGEFRONT, encryptor.encrypt(cardModel.getImageFront(), Utils.key));
+			values.put(DBConstants.COL_CARDDETAILS_IMAGEBACK, encryptor.encrypt(cardModel.getImageBack(), Utils.key));
 			
 			if ( isEdit ) {
 				database.update(DBConstants.TBL_CARDDETAILS, values, DBConstants.ID + "=?",
@@ -143,4 +167,44 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 		}
 		return -1;
 	}
+	
+	private final Encryptor PADDING_ENCRYPTOR = new Encryptor() {
+
+        @Override
+        public SecretKey deriveKey(String password, byte[] salt) {
+            return Crypto.deriveKeyPad(password);
+        }
+
+        @Override
+        public String encrypt(String plaintext, String password) {
+            key = deriveKey(password, null);
+
+            return Crypto.encrypt(plaintext, key, null);
+        }
+
+        @Override
+        public String decrypt(String ciphertext, String password) {
+            SecretKey key = deriveKey(password, null);
+
+            return Crypto.decryptNoSalt(ciphertext, key);
+        }
+    };
+    
+    abstract class Encryptor {
+        SecretKey key;
+
+        abstract SecretKey deriveKey(String passpword, byte[] salt);
+
+        abstract String encrypt(String plaintext, String password);
+
+        abstract String decrypt(String ciphertext, String password);
+
+        String getRawKey() {
+            if (key == null) {
+                return null;
+            }
+
+            return Crypto.toHex(key.getEncoded());
+        }
+    }
 }

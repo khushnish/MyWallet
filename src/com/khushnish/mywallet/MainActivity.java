09eec4bb7010320bef4affbb7d73ecbd23b7c757
khushnish <khushnishshah@gmail.com>
2013-12-16 00:02:22 +0530

    Update MainActivity.java

--- src/com/khushnish/mywallet/MainActivity.java
+++ src/com/khushnish/mywallet/MainActivity.java
@@ -1,10 +1,14 @@
 package com.khushnish.mywallet;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 
+import com.khushnish.mywallet.fragment.AddCardDetailsFragment;
 import com.khushnish.mywallet.fragment.ListDetailsFragment;
 
 public class MainActivity extends ActionBarActivity  {
@@ -29,5 +33,19 @@ public class MainActivity extends ActionBarActivity  {
 	public void hideBackButton() {
 		getSupportActionBar().setHomeButtonEnabled(false);
 		getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+	}
+	
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		
+		Log.e("Activity", "Activity : onActivityResult() called : requestCode : " + requestCode
+				+", resultCode : " + resultCode);
+		
+		if ( requestCode == 200 ) {
+			Fragment fragment = getSupportFragmentManager().
+					findFragmentByTag(AddCardDetailsFragment.class.getSimpleName());
+			fragment.onActivityResult(requestCode, resultCode, data);
+		}
 	}
 }

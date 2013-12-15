959c1167e950d85213cd651909f3eaafd76acce5
khushnish <khushnishshah@gmail.com>
2013-12-16 00:08:37 +0530

    Update SettingsFragment.java

--- src/com/khushnish/mywallet/fragment/SettingsFragment.java
+++ src/com/khushnish/mywallet/fragment/SettingsFragment.java
@@ -1,13 +1,14 @@
 package com.khushnish.mywallet.fragment;
 
-import com.khushnish.mywallet.MainActivity;
-import com.khushnish.mywallet.R;
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.khushnish.mywallet.MainActivity;
+import com.khushnish.mywallet.R;
 
 public class SettingsFragment extends Fragment {
 	
@@ -16,8 +17,19 @@ public class SettingsFragment extends Fragment {
 			Bundle savedInstanceState) {
 		final View view = inflater.inflate(R.layout.fragment_settings, null);
 		
+		setHasOptionsMenu(true);
 		((MainActivity) getActivity()).showBackButton();
 		
 		return view;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		
+		if (item.getItemId() == android.R.id.home) {
+	        getFragmentManager().popBackStack();
+	        return true;
+	    }
+		return super.onOptionsItemSelected(item);
 	}
 }
